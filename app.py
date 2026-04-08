727c62f28a7edeb198c8b84af52334d058559605
ROMndlkcndA <happyminecrafter8@gmail.com>
2026-04-08 08:23:07 +0300

    add main.db

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request
+import sqlite3	
 
+
+conn = sqlite3.connect("main.db")
 app = Flask(__name__)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS todo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        priority TEXT NOT NULL,
+        task_type TEXT NOT NULL,
+        status INTEGER DEFAULT 0
+    )
+""")
+conn.commit()
 
 @app.route("/", methods=["POST", "GET"])
 def new_todo():
@@ -13,7 +29,10 @@ def new_todo():
         message = f"Added: {todo_item} ({todo_priority})"
         print(request.form)
         
+
+        
     return render_template('index.html', message=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
+conn.close()
